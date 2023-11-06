be83b156e005af3b0d94eabc92cbd92f6901d3da
Selim Deniz Zayene <denizselimzayene@gmail.com>
2023-11-06 23:29:36 +0100

    tests and jenkinsfile

---
+++ Jenkinsfile
@@ -0,0 +1,9 @@
+pipeline {
+  agent any
+
+  stages{
+    stage('Test'){
+      sh './mvnw test'
+    }
+  }
+}
---
+++ src/test/java/tn/esprit/spring/SubscriptionServicesImplTest.java
@@ -0,0 +1,59 @@
+package tn.esprit.spring;
+
+import java.util.Optional;
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import tn.esprit.spring.entities.Subscription;
+import tn.esprit.spring.entities.TypeSubscription;
+import tn.esprit.spring.repositories.ISubscriptionRepository;
+import tn.esprit.spring.services.SubscriptionServicesImpl;
+
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+class SubscriptionServicesImplTest {
+  @Mock
+  ISubscriptionRepository subscriptionRepository;
+  
+  @InjectMocks
+  SubscriptionServicesImpl subscriptionServicesImpl;
+
+  Subscription subscription = new Subscription(1L, LocalDate.now(), LocalDate.of(2025, 1, 1), 10f,
+      TypeSubscription.ANNUAL);
+  
+  @Test
+  public void testAddSubscription() {
+    Mockito.when(subscriptionRepository.save(subscription)).thenReturn(subscription);
+
+    Subscription subscription1 = subscriptionServicesImpl.addSubscription(subscription);
+
+    Assertions.assertNotNull(subscription1);
+  }
+
+  @Test
+  public void testRetrieveSubscriptionById() {
+    Mockito.when(subscriptionRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(subscription));
+
+    Subscription subscription1 = subscriptionServicesImpl.retrieveSubscriptionById(1L);
+
+    Assertions.assertNotNull(subscription1);
+  }
+
+  @Test
+  public void testUpdateSubscription() {
+    Mockito.when(subscriptionRepository.save(subscription)).thenReturn(subscription);
+
+    Subscription subscription1 = subscriptionServicesImpl.updateSubscription(subscription);
+
+    Assertions.assertNotNull(subscription1);
+  }
+}
