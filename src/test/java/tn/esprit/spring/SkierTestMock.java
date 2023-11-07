8476018fd11d91d1b826cc6c3c486b2d11b4ad3d
Ahmed Aziz GHADHAB <ahmedaziz.ghadhab@esprit.tn>
2023-11-07 03:16:09 +0100

    Aziz Commit

---
+++ src/test/java/tn/esprit/spring/SkierTestMock.java
@@ -0,0 +1,38 @@
+package tn.esprit.spring.entities;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import java.time.LocalDate;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestSkierMock {
+    @InjectMocks
+    private SkierService SkierServiceImpl;
+
+    @Mock
+    private SkierRepository ISkierRepository;
+
+    @Test
+    public void testUpdateSkier() {
+        Skier skier = new Skier();
+        skier.setNumSkier(1L);
+        skier.setFirstName("Updated First Name");
+        skier.setLastName("Updated Last Name");
+        skier.setDateOfBirth(LocalDate.of(1995, 8, 20));
+        skier.setCity("Updated City");
+
+        when(skierRepository.save(skier)).thenReturn(skier);
+
+        Skier result = skierService.updateSkier(skier);
+
+        assertEquals(skier.getFirstName(), result.getFirstName());
+        assertEquals(skier.getLastName(), result.getLastName());
+        assertEquals(skier.getDateOfBirth(), result.getDateOfBirth());
+        assertEquals(skier.getCity(), result.getCity());
+    }
+}
---
+++ src/test/java/tn/esprit/spring/skierTest.java
@@ -0,0 +1,37 @@
+package tn.esprit.spring.entities;
+
+import org.junit.runner.RunWith;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class TestSkier {
+    @Autowired
+    private SkierService SkierServiceImpl;
+
+    @Autowired
+    private SkierRepository ISkierRepository;
+
+    @Test
+    public void testAddSkier() {
+        Skier skier = new Skier();
+        skier.setFirstName("John");
+        skier.setLastName("Doe");
+        skier.setDateOfBirth(LocalDate.of(1990, 5, 15));
+        skier.setCity("New York");
+
+        Skier result = skierService.addSkier(skier);
+
+        Skier fetchedSkier = skierRepository.findById(result.getNumSkier()).orElse(null);
+        assertEquals(skier.getFirstName(), fetchedSkier.getFirstName());
+        assertEquals(skier.getLastName(), fetchedSkier.getLastName());
+        assertEquals(skier.getDateOfBirth(), fetchedSkier.getDateOfBirth());
+        assertEquals(skier.getCity(), fetchedSkier.getCity());
+    }
+}
